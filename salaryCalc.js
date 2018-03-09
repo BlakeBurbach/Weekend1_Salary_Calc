8835fa7c2d07246c67f427959becc5b170f95566
Blake Burbach <cras.bburbach@gmail.com>
2018-03-09 16:07:28 -0600

    More folder building before tackling the assignment.

---
+++ salaryCalc.js
@@ -0,0 +1,16 @@
+console.log( 'js') ;
+
+// doc ready
+$( document ).ready( readyNow );
+
+// when doc is ready
+function readyNow(){
+  console.log( 'jQuery' );
+
+}
+
+class Employee {
+  constructor(){
+    
+  }
+}
