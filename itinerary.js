c8333ed387266c2969701b55b169de29d1fb84f6
Blake Burbach <cras.bburbach@gmail.com>
2018-03-09 17:42:29 -0600

    Got the inputs and button to accept employee info, create a new employee object, and push it to an array of employees.

---
+++ itinerary.js
@@ -0,0 +1,16 @@
+//[x] make a class called Employee with all the employee information
+
+// create a function that will create a new employee with information that it is given
+
+// [x] create a table that will house the employee information on the DOM
+
+// create a button that when clicked will
+// create and append a new employee to the table on the DOM with
+//information that is put into the input fields
+
+
+//create a function that will calculate the company's monthly cost according to
+//each employees annual salary
+
+//create a function that will turn the company's total monthly cost ready
+//when the company monthly cost goes over company's monthly budget
--- salaryCalc.js
+++ salaryCalc.js
@@ -1,16 +1,37 @@
-console.log( 'js') ;
-
 // doc ready
 $( document ).ready( readyNow );
 
-// when doc is ready
-function readyNow(){
-  console.log( 'jQuery' );
-
-}
-
+// class to create employee objects
 class Employee {
-  constructor(){
-    
+  constructor( firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryIn ){
+    this.firstName = firstNameIn;
+    this.lastName = lastNameIn;
+    this.idNumberIn = idNumberIn;
+    this.jobTitleIn = jobTitleIn;
+    this.annualSalaryIn = annualSalaryIn;
   }
 }
+
+
+let employeeArray = [];
+
+// when doc is ready
+function readyNow(){
+
+  // on click the submit button create an employee and add it to the employeeArray
+  function addEmployeeOnClick(){
+    $( '#submitBtn' ).on( 'click', function(){
+      console.log( 'button is working' );
+      let updatedEmployee = new Employee( $( '#firstNameInput' ).val(),
+                                          $( '#lastNameInput' ).val(),
+                                          $( '#idNumberInput' ).val(),
+                                          $( '#jobTitleInput' ).val(),
+                                          $( '#annualSalaryInput' ).val() );
+      employeeArray.push( updatedEmployee );
+      console.log( employeeArray );
+      return updatedEmployee;
+    }); // end on click
+  } // end addEmployeeOnClick
+
+  function 
+} // end readyNow
