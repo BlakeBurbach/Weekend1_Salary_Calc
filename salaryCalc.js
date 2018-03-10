f41832ba2f96246e91b3e3970e358dbaffd96009
Blake Burbach <cras.bburbach@gmail.com>
2018-03-10 13:48:43 -0600

    Was able to create an employee object and simultaneously push it to an array and append it to the table on the DOM.

--- salaryCalc.js
+++ salaryCalc.js
@@ -1,5 +1,4 @@
-// doc ready
-$( document ).ready( readyNow );
+console.log( 'js' );
 
 // class to create employee objects
 class Employee {
@@ -15,23 +14,75 @@ class Employee {
 
 let employeeArray = [];
 
+// doc ready
+$( document ).ready( readyNow );
+
 // when doc is ready
 function readyNow(){
+  console.log('jquery');
 
-  // on click the submit button create an employee and add it to the employeeArray
-  function addEmployeeOnClick(){
-    $( '#submitBtn' ).on( 'click', function(){
-      console.log( 'button is working' );
-      let updatedEmployee = new Employee( $( '#firstNameInput' ).val(),
-                                          $( '#lastNameInput' ).val(),
-                                          $( '#idNumberInput' ).val(),
-                                          $( '#jobTitleInput' ).val(),
-                                          $( '#annualSalaryInput' ).val() );
-      employeeArray.push( updatedEmployee );
-      console.log( employeeArray );
-      return updatedEmployee;
-    }); // end on click
-  } // end addEmployeeOnClick
+  // these two functions create ONE employee object from the Employee class
+  // pushEmployeeOnClick() will push it to the employeeArray
+  // appendEmployeeOnClick() will append to the employee table
+  pushEmployeeOnClick();
+  appendEmployeeOnClick();
 
-  function 
+
+  clearInputFields();
+
+
 } // end readyNow
+
+// on click the submit button create an employee and add it to the employeeArray
+function pushEmployeeOnClick(){
+  $( '#submitBtn' ).on( 'click', function(){
+    console.log( 'button is working' );
+    //this updatedEmployee is the same employee object that is created with appendEmployeeOnClick()
+    let updatedEmployee = new Employee( $( '#firstNameInput' ).val(),
+                                        $( '#lastNameInput' ).val(),
+                                        $( '#idNumberInput' ).val(),
+                                        $( '#jobTitleInput' ).val(),
+                                        $( '#annualSalaryInput' ).val() );
+    employeeArray.push( updatedEmployee );
+    console.log( employeeArray );
+    return updatedEmployee;
+  }); // end on click
+} // end addEmployeeOnClick
+
+// function that will create a new employee object and append it to the table DOM
+function appendEmployeeOnClick(){
+  // on click create a new employee object
+  $( '#submitBtn' ).on( 'click', function(){
+    console.log( 'button is working' );
+    // this updatedEmployee is the same employee object that is created in pushEmployeeOnClick()
+    let updatedEmployee = new Employee( $( '#firstNameInput' ).val(),
+                                        $( '#lastNameInput' ).val(),
+                                        $( '#idNumberInput' ).val(),
+                                        $( '#jobTitleInput' ).val(),
+                                        $( '#annualSalaryInput' ).val() );
+    // variables that will house the input values for ease of rewriting
+    let first = $( '#firstNameInput' ).val();
+    let last = $( '#lastNameInput' ).val();
+    let id = $( '#idNumberInput' ).val();
+    let jobTitle = $( '#jobTitleInput' ).val();
+    let salary = $( '#annualSalaryInput' ).val();
+    // append the new employee info to the table
+    $( '#employeeTable' ).append( '<tr><td>' + first + '</td>' +
+                                  '<td>' + last + '</td>' +
+                                  '<td>' + id + '</td>' +
+                                  '<td>' + jobTitle + '</td>' +
+                                  '<td>' + salary + '</td></tr>' );
+  }); // end create employee on click
+} // end appendEmployeeToTable
+
+//this function will clear the input field after a submission has been made
+function clearInputFields(){
+  // once these functions have been run, clear the input fields
+  if ( addEmployeeOnClick === true && appendEmployeeToTable === true ){
+    return $( '#firstNameInput' ).val( '' ),
+           $( '#lastNameInput' ).val( '' ),
+           $( '#idNumberInput' ).val( '' ),
+           $( '#jobTitleInput' ).val( '' ),
+           $( '#annualSalaryInput' ).val( '' );
+  } // end if
+} // end clearInputFields
